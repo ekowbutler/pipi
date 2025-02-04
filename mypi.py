51fd92b2213b328c425fb2fa1f700d2f1be91f38
ekowbutler <ekowbutler@gmail.com>
2025-02-04 23:43:58 +0000

    first line added

--- mypi.py
+++ mypi.py
@@ -1,3 +1,18 @@
-n = int(input("Number: "))
-if n > 0:
-    print("n is positive")
+class Flight:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+
+    def add_passenger(self, name):
+        if not self.open_seats():
+            return False
+        self.passengers.append(name)
+        return True
+
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+
+
+flight = Flight(3)
+
+people = ["harry", "leo", "john", "gina"]
